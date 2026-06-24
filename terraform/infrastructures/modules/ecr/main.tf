b4961c8f848a6fa6e530e2d83ece411407ed38cc
Baibhav Dhungana <baibhav.dhungana@truenary.com>
2026-06-25 02:14:48 +0545

    set ecr images to force delte

--- terraform/infrastructures/modules/ecr/main.tf
+++ terraform/infrastructures/modules/ecr/main.tf
@@ -3,14 +3,17 @@
 resource "aws_ecr_repository" "vote-repository" {
   name                 = "voting-app/vote"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "worker-repository" {
   name                 = "voting-app/worker"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "result-repository" {
   name                 = "voting-app/result"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
